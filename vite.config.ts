84df39726cd9a20622485491450a1d62b59271ac
Pablo <juan.mejia@simetrik.com>
2025-10-20 09:08:07 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bold-react/', // Configuración para GitHub Pages
   server: {
     port: 3000,
     open: true
